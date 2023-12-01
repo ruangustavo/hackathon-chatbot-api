c514f66abeff30df6d94c59fb0c6f41840eb185d
Ruan Gustavo <ruan.gustavo@escolar.ifrn.edu.br>
2023-12-01 17:05:05 -0300

    feat: add new files and functions for WebSocket and promotions

---
+++ app/promotions.py
@@ -0,0 +1,56 @@
+import httpx
+from typing import Optional, Dict
+from urllib.parse import urljoin
+
+PECHINCHOU_BASE_URL = "https://admin.pechinchou.com.br/api/v2/"
+PECHINCHOU_SEARCH_URL = urljoin(
+    PECHINCHOU_BASE_URL,
+    "produto/listar_produtos_por_opcao/search/{product}/?page=1",
+)
+
+
+def search_promotions(product: str) -> Optional[Dict]:
+    url = PECHINCHOU_SEARCH_URL.format(product=product)
+    response = httpx.get(url).json()
+    return response.get("results")
+
+
+def filter_active_promotions(promotions):
+    for promotion in promotions:
+        old_price = float(promotion["old_price"])
+        price = float(promotion["price"])
+        promotion["price_discount"] = int(99 - ((price * 100) / old_price))
+        promotion["total_likes"] = len(promotion["likes"])
+
+    active_promotions = [
+        product
+        for product in promotions
+        if product["status"] == "ACTIVE" and not product["warning"]
+    ]
+
+    return active_promotions
+
+
+def get_best_promotion(product):
+    print(f"get_best_promotion {product}")
+    promotions = search_promotions(product)
+
+    if promotions is None or promotions == []:
+        return {
+            "products": [],
+        }
+
+    active_promotions = filter_active_promotions(promotions)
+
+    # Ordenando as promoções por likes em ordem decrescente
+    active_promotions.sort(
+        key=lambda x: x["total_likes"],
+        reverse=True,
+    )
+
+    best_promotion = active_promotions[0]
+
+    return {
+        "products": active_promotions,
+        "best_product": best_promotion,
+    }
---
+++ app/websocket.py
@@ -0,0 +1,18 @@
+import json
+from fastapi import WebSocket
+from typing import List
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(json.dumps(message))
